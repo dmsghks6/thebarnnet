03d73c8253232d9a599c2b58b78bcc1901ac025b
park eunhwan <dmsghks6@gmail.com>
2023-02-28 17:36:52 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,209 @@
+$(document).ready(function(){
+
+
+    $(window).scroll(function(){
+        const sct = $(window).scrollTop();
+
+
+        if(sct >= 500){
+            $('.banner-empty .header-area').addClass('on'); 
+            // 헤더의 위에 부분 없어지게 하기
+
+        }else {
+            $('.banner-empty .header-area').removeClass('on');   
+
+        }
+
+
+    });
+
+    $('.menu-zone h3').click(function(){
+        $('.menu-area').toggleClass('on');
+    });
+
+
+
+    
+    $('.menu-area .menu').click(function(){
+        console.log('dd')
+        $(this).children().stop().slideDown();
+    });
+
+
+    $('.menu-area .menu').mouseenter(function(){
+        // $('.sub-menu').stop().slideDown();
+        // 수정
+        $(this).siblings('.sub-menu').stop().slideDown();
+        $(this).parent('.menu-area li').siblings().children('.sub-menu').stop().slideUp();
+
+    });
+
+    $('.menu-area > li').mouseleave(function(){
+        $('.sub-menu').stop().slideUp();
+        // 수정
+
+    });
+
+   
+
+
+    // 추가
+    $(window).scroll(function(){
+        const empty3 = $(".empty3").offset().top+100;
+        const sct = $(window).scrollTop();
+        console.log(sct)
+
+        if(sct >= empty3){
+            console.log('h')
+            $('.banner .video-area').addClass('on');
+      
+        }else{
+            $('.banner .video-area').removeClass('on');
+            
+
+        }
+    });
+
+
+    gsap.registerPlugin(ScrollTrigger);
+        
+    ScrollTrigger.defaults({
+    markers: false
+    });
+
+    // sticky-video
+    $(".banner-empty").each(function (index) {
+        let triggerElement = $(this);
+        let targetElement = $(".sticky-video-element");
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+            trigger: triggerElement,
+            // trigger element - viewport
+            start: "top top",
+            end: "bottom bottom",
+            scrub: 3,
+            }
+        });
+        tl.fromTo(targetElement, {
+                width: "523px",
+                height: "429px",
+                borderRadius: "15px",
+            },
+            {
+                width: "100vw",
+                height: "100vh",
+                borderRadius: "0",
+                duration: 3,
+
+            },
+        );
+    });
+
+
+
+
+    $('.menu-area .menu').mouseleave(function(){
+        $(this).children('.sub-menu').stop().slideUp();
+    });
+
+
+
+    // $('.sec-3 .left-area').mouseleave(function(e){ 
+    //      TweenMax.to(this, 0.3, {height: 150, width: 150}); 
+    //       TweenMax.to('.model1', 0.3,{scale:1, x: 0, y: 0});
+    // });
+
+    // $('.sec-3 .left-area').mouseenter(function(e){ 
+    //      TweenMax.to(this, 0.3, {height: 200, width: 200});  
+    //      TweenMax.to('.model1', 0.3,{scale:1.3});
+    //     });
+        
+    // $('.sec-3 .left-area').mousemove(function(e){     
+    //     callParallax(e);
+    // });
+    
+    // function callParallax(e){  
+    //     parallaxIt(e, '.model1', 80); 
+    // }
+     
+    // function parallaxIt(e, target, movement){  
+    //     var $this = $('.sec-3 .left-area');  
+    //     var relX = e.pageX - $this.offset().left;  
+    //     var relY = e.pageY - $this.offset().top;    
+
+    //     TweenMax.to(target, 0.3, {   
+    //         x: (relX - $this.width()/2) / $this.width() * movement,    
+    //         y: (relY - $this.height()/2) / $this.height() * movement,    ease: Power2.easeOut  
+    //     });
+    // }
+
+
+    $('.sec-3 .left-area').mouseleave(function(e){ 
+         TweenMax.to(this, 0.3, {height: 1080,  width:605}); 
+          TweenMax.to('.model1', 0.3,{scale:1, x: 0, y: 0});
+    });
+
+    $('.sec-3 .left-area').mouseenter(function(e){ 
+         TweenMax.to(this, 0.3, {height: 1080  , width:605});  
+         TweenMax.to('.model1', 0.3,{scale:1.2});
+     });
+        
+    $('.sec-3 .left-area').mousemove(function(e){     
+        callParallax(e);
+    });
+    
+    function callParallax(e){  
+        parallaxIt(e, '.model1', 200); 
+    }
+     
+    function parallaxIt(e, target, movement){  
+        var $this = $('.sec-3 .left-area');  
+        var relX = e.pageX - $this.offset().left;  
+        var relY = e.pageY - $this.offset().top;    
+
+        TweenMax.to(target, 0.3, {   
+            x: (relX - $this.width()/2) / $this.width() * movement,    
+            y: (relY - $this.height()/2) / $this.height() * movement,    
+            ease: Power2.easeOut  
+        });
+    }
+
+
+
+    $('.sec-4 .right-area').mouseleave(function(w){ 
+        TweenMax.to(this, 0.5, {height: 1080, width: 605}); 
+         TweenMax.to('.model2', 0.5,{scale:1, x: 0, y: 0});
+   });
+
+   $('.sec-4 .right-area').mouseenter(function(w){ 
+        TweenMax.to(this, 0.5, {height: 1080  , width: 605});  
+        TweenMax.to('.model2', 0.5,{scale:1.2});
+    });
+       
+   $('.sec-4 .right-area').mousemove(function(w){     
+       callParallax2(w);
+   });
+   
+   function callParallax2(w){  
+       parallaxIt2(w, '.model2', 200); 
+   }
+    
+   function parallaxIt2(w, target, movement){  
+       var $this = $('.sec-4 .right-area');  
+       var relX2 = w.pageX - $this.offset().left;  
+       var relY2 = w.pageY - $this.offset().top;    
+
+       TweenMax.to(target, 0.5, {   
+           x: (relX2 - $this.width()/2) / $this.width() * movement,    
+           y: (relY2 - $this.height()/2) / $this.height() * movement,    
+           ease: Power2.easeOut  
+       });
+   }
+
+
+
+   AOS.init();
+
+
+});
